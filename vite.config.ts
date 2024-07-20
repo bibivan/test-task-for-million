3effd35ee874db5a69bc5d700e55574ba36b2342
Ivan Bibinin <i.bibinin@notamedia.com>
2024-07-20 07:46:30 +0200

    some fixed more

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/million-test-task/',
+  base: '/test-task-for-million/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
