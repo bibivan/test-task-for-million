d4c1a6f98a372977ef9517308d20d179bb7189df
Ivan Bibinin <i.bibinin@notamedia.com>
2024-07-20 07:43:31 +0200

    path fixed

--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './common'
-export * from './currencies'
-export * from './payment'
-export * from './store'
--- src/types/store.ts
+++
@@ -1,7 +0,0 @@
-import type { TNullable } from '@/types'
-
-export type TDefaultState<T extends any = any> = {
-  data: TNullable<T>
-  loading: boolean
-  error: TNullable<Error>
-}
